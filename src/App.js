d87c428bf793c2bff6f04299fa9c5ab708a37d83
pkuphy <3149388+pkuphy@users.noreply.github.com>
2019-12-02 09:24:32 +0100

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
-import { Button } from 'antd';
+import { Button, Icon } from 'antd';
 import './App.css';
 
 const ButtonGroup = Button.Group;
 
 function App() {
   const [count, setCount] = useState(0);
+  const step = 1;
 
   return (
     <div className="App">
       <div>
         <p>计数器数值：{count}</p>
         <ButtonGroup>
-          <Button type="primary" onClick={() => setCount(count + 1)}>计数器 +1</Button>
-          <Button type="primary" onClick={() => setCount(count - 1)}>计数器 -1</Button>
+          <Button type="danger" onClick={() => setCount(count - step)}><Icon type="minus-circle" />计数器 -{`${step}`}</Button>
+          <Button type="primary" onClick={() => setCount(count + step)}><Icon type="plus-circle" />计数器 +{`${step}`}</Button>
         </ButtonGroup>
       </div>
     </div>
